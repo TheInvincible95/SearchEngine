8f8eba2f37b38b3cdab5cd71ae375768b0b930dd
QuickAxe <106764539+ManselInfinity@users.noreply.github.com>
2024-04-12 19:02:16 +0530

    fisrt start is now sped up, using previously computed TF-IDF vectors

--- search_engine.py
+++ search_engine.py
@@ -14,29 +14,14 @@ import sys
 import corpusToDict as ctd
 import search as sch
 
-# documents = {
-#     "0": "Artificial intelligence has revolutionized various industries.",
-#     "1": "The future of technology is driven by artificial intelligence.",
-#     "2": "Machine learning and AI are transforming businesses worldwide.",
-#     "3": "Advancements in robotics have paved the way for automation.",
-#     "4": "The role of data analytics in shaping the future of technology.",
-#     "5": "Healthy eating habits are essential for a balanced lifestyle.",
-#     "6": "Nutritious foods contribute to overall well-being and longevity.",
-#     "7": "The importance of incorporating fruits and vegetables into your diet.",
-#     "8": "Exploring different cuisines can be a delightful culinary experience.",
-#     "9": "Cooking techniques and recipes for preparing delicious meals at home.",
-#     "10": "Space exploration continues to unravel the mysteries of the universe.",
-#     "11": "The race to Mars: Challenges and opportunities for human colonization.",
-#     "12": "The wonders of astronomy: Discovering new celestial phenomena.",
-#     "13": "Exploring the vastness of the cosmos through powerful telescopes.",
-#     "14": "The impact of social media on modern communication and relationships.",
-#     "15": "Navigating the complexities of social media etiquette in the digital age.",
-#     "16": "The rise of influencer culture: Examining its implications on society.",
-#     "17": "Effective strategies for managing stress and promoting mental well-being.",
-#     "18": "Mindfulness and meditation practices for achieving inner peace and clarity.",
-#     "19": "The importance of self-care in maintaining physical and emotional health.",
-# }
+# to store the state of the searcher class, so as to not recompute it everytime the code runs
+import pickle
+import os
 
+# ============================================================== Run this when file is imported, as initialisation ==================================================================
+
+# this part takes ~48ms, so dont store it, and compute it each time the code is run
+# ---------------------------------------------------------------------------------
 # getting the corpus here:
 filePath = "./Corpus.csv"
 
@@ -49,12 +34,30 @@ for i in range(1, len(corpus)):
     text = corpus[i][0]
     documents[i - 1] = text
 
-# Create a Searcher instance
-search_engine = sch.Searcher()
+# ---------------------------------------------------------------------------------
 
-for name, doc in documents.items():
-    search_engine.add_document(doc)
-search_engine.avgdlcalc()
+# This part takes about 6s
+# if there is no pickle of a previously computed searcher class, compute it and store it in a pickle
+# reduces the runtime of this part of code from ~6s to ~1s
+if not os.path.exists("./searcherPickle.pkl"):
+
+    # Create a Searcher instance
+    search_engine = sch.Searcher()
+
+    for name, doc in documents.items():
+        search_engine.add_document(doc)
+    search_engine.avgdlcalc()
+
+    # create the pickle file
+    with open("searcherPickle.pkl", "wb") as file:
+        pickle.dump(search_engine, file)
+
+# if the searcher class already computed stuff, and stored it in a pickle, directly read the searcher class from the pickle:
+else:
+    with open("searcherPickle.pkl", "rb") as file:
+        search_engine = pickle.load(file)
+
+# ===================================================================================================================================================================================
 
 # return the results of a query
 
